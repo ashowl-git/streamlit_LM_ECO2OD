177181b97e47ebaf1d22a326d59ef22cf0226e29
ashowl-git <ashaahn@gmail.com>
2023-03-08 15:39:40 +0900

    e

--- streamlit_LM_ECO2OD_main.py
+++ streamlit_LM_ECO2OD_main.py
@@ -50,28 +50,28 @@ from sklearn.metrics import mean_squared_log_error
 
 
 
-import streamlit as st
+# import streamlit as st
 
-def main_page():
-    st.markdown("# Main page 🎈")
-    st.sidebar.markdown("# Main page 🎈")
+# def main_page():
+#     st.markdown("# Main page 🎈")
+#     st.sidebar.markdown("# Main page 🎈")
 
-def page2():
-    st.markdown("# Page 2 ❄️")
-    st.sidebar.markdown("# Page 2 ❄️")
+# def page2():
+#     st.markdown("# Page 2 ❄️")
+#     st.sidebar.markdown("# Page 2 ❄️")
 
-def page3():
-    st.markdown("# Page 3 🎉")
-    st.sidebar.markdown("# Page 3 🎉")
+# def page3():
+#     st.markdown("# Page 3 🎉")
+#     st.sidebar.markdown("# Page 3 🎉")
 
-page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
-}
+# page_names_to_funcs = {
+#     "Main Page": main_page,
+#     "Page 2": page2,
+#     "Page 3": page3,
+# }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+# selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+# page_names_to_funcs[selected_page]()
 
 
 # # hide the hamburger menu?
