0a0dd13bf81e78249677715fbec8fb6818145727
ashowl-git <ashaahn@gmail.com>
2023-03-08 15:23:30 +0900

    first commit

---
+++ streamlit_LM_ECO2OD_main.py
@@ -0,0 +1,100 @@
+# 분석전에 필요한 라이브러리들을 불러오기
+# 테스트
+# plotly라이브러리가 없다면 아래 설치
+# conda install -c plotly plotly=4.12.0
+# conda install -c conda-forge cufflinks-py
+# conda install seaborn
+
+import glob 
+import os
+import sys, subprocess
+from subprocess import Popen, PIPE
+import numpy as np
+import pandas as pd
+
+import streamlit as st
+import sklearn
+import seaborn as sns
+# sns.set(font="D2Coding") 
+# sns.set(font="Malgun Gothic") 
+# from IPython.display import set_matplotlib_formats
+# set_matplotlib_formats("retina")
+import matplotlib.pyplot as plt
+import plotly.io as pio
+import plotly.express as px
+import plotly.graph_objects as go 
+import chart_studio.plotly as py
+import cufflinks as cf
+# # get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# # Make Plotly work in your Jupyter Notebook
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+# init_notebook_mode(connected=True)
+# # Use Plotly locally
+cf.go_offline()
+
+
+# 사이킷런 라이브러리 불러오기 _ 통계, 학습 테스트세트 분리, 선형회귀등
+from scipy import stats
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error 
+from sklearn.metrics import r2_score 
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
+from sklearn.metrics import mean_squared_log_error
+
+
+
+
+
+
+import streamlit as st
+
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
+
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
+
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
+
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
+
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
+
+# # hide the hamburger menu?
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: visible;}
+        footer:after {content:'Copyright @ 2023: EAN technology all rights reserved';
+        display:block;
+        opsition:relatiive;
+        color:tomato;
+        padding:5px;
+        top:100px;}
+
+        </style>
+        """
+
+st.set_page_config(layout="wide", page_title="국토안전관리원_온실가스감축계수")
+st.markdown(hide_menu_style, unsafe_allow_html=True) # hide the hamburger menu?
+
+
+
+
+
+
+
