e79fe7b45d44356f98b64cae3a0bf639dc78c057
ashowl-git <ashaahn@gmail.com>
2023-03-21 12:58:12 +0900

    e

--- pages/03_병원.py
+++ pages/03_병원.py
@@ -588,6 +588,7 @@ df_input_all = pd.concat([df_input, df2_input], axis=0)
 df_input_all['Alt'] = ['Base','Alt']
 df_input_all = df_input_all.set_index(df_input_all['Alt'])
 df_input_all_T = df_input_all.T
+df_input_all_T.drop(['Alt'], axis=0, inplace=True)
 df_input_all_T
 
 
